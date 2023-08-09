a3684aa5cb945758bae0412ecb203ef7b6a274c9
Suryansh Gupta <suryanshgupta133@gmail.com>
2023-08-09 16:14:05 +0530

    Add Post, User and Authentocation via devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+  get '/u/:id', to: 'users#profile', as: 'user'
   resources :posts
   get 'pages/about', to: 'pages#about'
   root "pages#home"
-end
+end  
